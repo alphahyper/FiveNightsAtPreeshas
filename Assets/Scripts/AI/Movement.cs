52734901ad0a7f788c4768242879233c5620dd2b
Jack Liang <s127085@ltisdschools.net>
2026-03-26 12:03:57 -0500

    Added Movement Script

---
+++ Assets/Scripts/AI/Movement.cs
@@ -0,0 +1,39 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using Random = UnityEngine.Random;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private int room;
+    private float timeLeft;
+    private float minTime, maxTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        room = 0;
+        minTime = 2f; maxTime = 10f;  // CHANGES TIMER
+        timeLeft = Random.Range(minTime, maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            moveRoom(ref room);
+            timeLeft = Random.Range(minTime, maxTime);
+        }
+    }
+
+    // Moves the AI to the next room
+    void moveRoom(ref int room)
+    {
+        Debug.Log($"Moved to room {room + 1}");
+        room++;
+    }
+}
