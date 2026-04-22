5884ea99c953769d6a3d187b325eb74833e1e2c6
Pedro Alvarez Gutierrez <s114605@ltisdschools.net>
2026-04-22 11:56:19 -0500

    4/22 Work, Added some transitions

--- Assets/Scripts/FadeScript.cs
+++ Assets/Scripts/FadeScript.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using static Unity.Collections.AllocatorManager;
 
+
 public class FadeScript : MonoBehaviour
 {
+    public static int nightNumber = 0;
     public Text myText;
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
+        nightNumber++;
+        myText.text = "Night " + nightNumber;
         // Fade to invisible (0 alpha) immediately
         myText.canvasRenderer.SetAlpha(0f);
         // Fade in to full (1 alpha) over 2 seconds
         Invoke(nameof(FadeIn), 1f);
         Invoke(nameof(FadeOut), 5f);
+        await Task.Delay(8000);
+        SceneManager.LoadScene("School");
     }
 
     public void FadeOut()
